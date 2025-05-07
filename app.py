3238419a100c014624092b245376d28290f88efb
Isobel Pang <173300585+isobel-p@users.noreply.github.com>
2025-05-07 13:51:40 +0100

    Create basic contact form

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template, abort, send_from_directory
+from flask import Flask, render_template, abort, send_from_directory, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+import os
+
+class Contact(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
 
 app = Flask(__name__)
+load_dotenv()
+app.config['SECRET_KEY'] = "Hello123"
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    form = Contact()
+    if form.validate_on_submit():
+        name = form.name.data
+        with open("test", "a") as file:
+            file.write(name + "\n")
+        return redirect('/success')
+    return render_template("contact.html", form=form)
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 @app.route('/about')
 def about():
