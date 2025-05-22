37f6187073eefe80124ccce00ea642732b17332a
Isobel Pang <173300585+isobel-p@users.noreply.github.com>
2025-05-22 13:30:02 +0100

    Create playground section

---
+++ static/script.js
@@ -0,0 +1,10 @@
+// split elements with the class "split" into words and characters
+let split = SplitText.create(".split", { type: "words, chars" });
+
+// now animate the characters in a staggered fashion
+gsap.from(split.chars, {
+  duration: 1, 
+  y: 100,       // animate from 100px below
+  autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+  stagger: 0.05 // 0.05 seconds between each
+});
