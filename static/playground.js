93f7772df001c2c74f3a85170fdd9815f5fceb03
Isobel Pang <173300585+isobel-p@users.noreply.github.com>
2025-05-23 14:26:18 +0100

    Create music button

--- static/playground.js
+++ static/playground.js
@@ -1,3 +1,21 @@
 window.addEventListener("load", (event) => {
   new cursoreffects.trailingCursor();
 });
+
+const audio = document.getElementById('music');
+const btn = document.getElementById('music-button');
+
+let playing = false;
+
+audio.src = 'https://cloud-dx9y4rk8f-hack-club-bot.vercel.app/5drunk_raccoon_audio.mp4';
+
+btn.addEventListener('click', () => {
+    playing = !playing;
+    if (playing) {
+    audio.play();
+    btn.textContent = '🥳';
+    } else {
+    audio.pause();
+    btn.textContent = '🤫';
+    }
+});
